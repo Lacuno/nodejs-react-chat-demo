50fb874068ba98190a77b6c90a2dc24a874b3f22
Christoph Weiler <mail@chrisweiler.at>
2020-06-19 09:24:01 +0200

    Add messages to chat via button click (for further frontend development
     - This will later change because we will get our chat messages via websockets from the (now inexistent) backend

--- frontend-react/src/components/Chat.tsx
+++ frontend-react/src/components/Chat.tsx
@@ -1,24 +1,44 @@
 import * as React from "react";
+import {ChatMessage, ChatMessageProps} from "./ChatMessage";
 
-export class Chat extends React.Component {
+interface ChatState {
+    messages: Array<ChatMessageProps>
+}
+
+export class Chat extends React.Component<{}, ChatState> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            messages: [{user: 'Chris', message: 'Hello there'}, {user: 'Chris', message: 'Hello there'}, {
+                user: 'Chris',
+                message: 'Hello there'
+            }]
+        }
+    }
+
+
     render() {
         const footerStyle = {
             height: '2em',
             padding: '3px',
         };
 
-        const messages: Array<ChatMessageProps> = [{user: 'Chris', message: 'Hello there'}, {user: 'Chris', message: 'Hello there'}, {user: 'Chris', message: 'Hello there'}];
-        const chatMessages = messages.map(message => {
-            return <ChatMessage user={message.user} message={message.message} />
+        const chatMessages = this.state.messages.map(message => {
+            return <ChatMessage user={message.user} message={message.message}/>
         });
 
         return <div className="full-screen column-layout">
-            <main className="column-layout stretch">
+            <main className="column-layout stretch overflow-y">
                 {chatMessages}
             </main>
             <footer style={footerStyle} className="row-layout">
                 <input className="stretch"/>
-                <button>Send</button>
+                <button onClick={() => this.setState(
+                    previousState => ({
+                        messages: [...previousState.messages, {user: 'Chris', message: 'Hello there'}]
+                    })
+                )}>Send
+                </button>
             </footer>
         </div>;
     }
